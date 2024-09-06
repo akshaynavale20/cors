ba279332f3717fdecf1a45ff4dbbb8207ac4be87
Akshay Navale <akshay.navale@opiesoftware.com>
2024-09-06 16:51:21 +0530

    Test123456

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const getOriginalUrl = (req) => {
 const addSecurityHeaders = (req, res, next) => {
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
 };
 
